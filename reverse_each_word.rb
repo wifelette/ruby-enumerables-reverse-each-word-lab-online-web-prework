5a795040d540bd9759cc9503236ba7c2b3d91379
Leah Silber <leahsilber@gmail.com>
2019-08-02 03:29:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(string)
-  new_string = string.split(" ")
-  reverse_string = ""
-  new_string.each do |i|
-    reverse_string += i.reverse + " "
-  end
-  reverse_string.strip
-end
+# def reverse_each_word(string)
+#   new_string = string.split(" ")
+#   reverse_string = ""
+#   new_string.each do |i|
+#     reverse_string += i.reverse + " "
+#   end
+#   reverse_string.strip
+# end
 
 def reverse_each_word(string)
   new_string = string.split(" ")
